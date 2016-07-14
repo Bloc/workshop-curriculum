8d8ecbd101168b987640685d65d14ce55a6b09bf
clln <colleen@bloc.io>
2016-07-14 16:57:46 -0700

    Add tests and solution for Scope Chain exercise

---
+++ courses/intermediate-javascript/01-scope/04-scope-chain/spec.js
@@ -0,0 +1,7 @@
+var expect = require("chai").expect;
+
+describe("goldenGateParkReturns", function() {
+  it("is assigned the number corresponding to the correct array", function() {
+    expect(goldenGateParkReturns).to.eql(3);
+  });
+});
