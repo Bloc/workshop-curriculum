cd5f4a8f884a011c8d3d4fe46bd309629fd16b76
Stanley Idesis <stan@bloc.io>
2016-07-15 18:31:58 +0000

    Patched up the Variables Exercise

--- courses/ruby-primer/01-level-getting-started/01-variables/starting_code.rb
+++ courses/ruby-primer/01-level-getting-started/01-variables/starting_code.rb
@@ -1,8 +1,28 @@
 def my_first_variables
-  days_in_a_year = # enter a value here
-  planet_i_live_on = # enter a value here
+  #
+  # 1
+  #
+  # assign your name here
+  my_name
+  # prints your name to the output
+  puts my_name
 
-  # The following line of code uses the variables that you assign. You do not need to modify it for this exercise.
+  #
+  # 2
+  #
+  # create and assign `my_age` here
+  # print `my_age` here
+
+  #
+  # 3
+  #
+  # assign a number below
+  days_in_a_year
+  # assign a string below
+  planet_i_live_on
+
+  # The following line of code uses the variables that you assign.
+  # You do not need to modify it for this exercise.
 
   "I am happy to live on planet #{planet_i_live_on} where there are #{days_in_a_year} days in a year!"
 end
