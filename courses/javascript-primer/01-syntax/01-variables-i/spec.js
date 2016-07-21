64bcfdc5d42b0a12f894b2cc8675e76549c1ef6c
clln <colleen@bloc.io>
2016-07-21 11:44:22 -0700

    JS Primer course updates

--- courses/javascript-primer/01-syntax/01-variables-i/spec.js
+++ courses/javascript-primer/01-syntax/01-variables-i/spec.js
@@ -1,7 +1,11 @@
 var assert = require('chai').assert;
 
 describe('numberOfStatesInUSA', function () {
-  it('equals the number of states in the USA', function () {
+  it('is defined', function () {
+    assert.isDefined(numberOfStatesInUSA);
+  });
+  it('is assigned a numerical value equal to the number of states in the USA', function () {
+    assert.isNumber(numberOfStatesInUSA);
     assert.equal(50, numberOfStatesInUSA);
   });
 });
