8db16ed8a20dc7ecb9450542dbe4eedafd010e4d
Stanley Idesis <stan@bloc.io>
2016-07-22 15:42:49 +0000

    Or Maybe We Go with This

--- courses/ruby-primer/03-level-conditionals/05-or/spec.rb
+++ courses/ruby-primer/03-level-conditionals/05-or/spec.rb
@@ -1,22 +1,33 @@
-describe "tweet_length" do
-  it "returns the character count difference if a tweet is less than 140 characters and the payment is $1.00 and the user is an employee" do
-    expect( tweet_length("A"*130, 1, true) ).to eq("You can use 10 more characters if you want!")
+describe "is_adult?" do
+  it "returns true for adults" do
+    expect( is_adult?(45) ).to eq(true)
   end
 
-  it "returns 'You tweeted a perfect tweet!' if a tweet is exactly 140 characters and the payment is $1.00 and the user is an employee" do
-    expect( tweet_length("A"*140, 1, true) ).to eq("You tweeted a perfect tweet!")
+  it "returns false for children" do
+    expect( is_adult?(5) ).to eq(false)
   end
 
-  it "returns the character count difference if a tweet is longer than 140 characters and the payment is $1.00 and the user is an employee" do
-    expect( tweet_length("A"*999, 1, true) ).to eq("You are 859 characters over your limit of 140!")
-  end
-
-  it "returns 'You must pay $1.00 for tweets now!' if a tweet is exactly 140 characters and the payment is not $1.00 and the user is not an employee" do
-    expect( tweet_length("A"*140, 0, false) ).to eq("You must pay $1.00 for tweets now!")
-  end
-
-  it "returns 'You tweeted a perfect tweet!' if a tweet is exactly 140 characters and the payment is not $1.00 and the user is an employee" do
-    expect( tweet_length("A"*140, 0, true) ).to eq("You tweeted a perfect tweet!")
+  it "returns false for the elderly" do
+    expect( is_adult?(65) ).to eq(false)
   end
 end
 
+describe "perform_search?" do
+  it "returns true for adults" do
+    expect( perform_search?(35, false) ).to eq(true)
+  end
+
+  it "returns false for children" do
+    expect( perform_search?(4, false) ).to eq(false)
+  end
+
+  it "returns false for the elderly" do
+    expect( perform_search?(68, false) ).to eq(false)
+  end
+
+  it "returns true as long as we do a random pat-down" do
+    expect( perform_search?(68, true) ).to eq(true)
+    expect( perform_search?(2, true) ).to eq(true)
+    expect( perform_search?(43, true) ).to eq(true)
+  end
+end
---
+++ courses/ruby-primer/03-level-conditionals/05-or/starting_code.rb
@@ -0,0 +1,9 @@
+#
+# 1
+#
+# implement `is_adult?` below
+
+#
+# 2
+#
+# implement `perform_search?` below
