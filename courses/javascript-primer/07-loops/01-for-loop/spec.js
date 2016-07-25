def3271547333f370edfe9d9c96e3200342c506f
Phillip Spitler <phil@bloc.io>
2016-07-25 09:48:38 -0400

    Changed spec file 'to' to 'through' for clarification.

--- courses/javascript-primer/07-loops/01-for-loop/spec.js
+++ courses/javascript-primer/07-loops/01-for-loop/spec.js
@@ -5,7 +5,7 @@ describe("addToArray", function() {
     expect(addToArray).to.exist;
   });
 
-  it("adds numbers from num1 to num2 to an array", function() {
+  it("adds numbers from num1 through num2 to an array", function() {
     expect(addToArray(2,6)).to.eql([2,3,4,5,6]);
     expect(addToArray(2,2)).to.eql([2]);
   });
