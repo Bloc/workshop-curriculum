5070a9f3fdc9b3dddb9c3c8e276406fc4cdd323d
Raj <rajeev.v.singh@gmail.com>
2016-08-25 17:16:50 -0400

    Fix bug in Js Primer / Syntax / Logical Operators exercise

--- courses/javascript-primer/01-syntax/18-logical-operators/solution.js
+++ courses/javascript-primer/01-syntax/18-logical-operators/solution.js
@@ -1,7 +1,7 @@
 <!--{ ids:[143], language:'JavaScript', type:'workshop', order: 17, name:'Logical Operators', description:'Test the relationship between two boolean values' } -->
 var logical1;
 var logical2;
-var logical3 = (true === 1) || (false === 0);
+var logical3 = (true == 1) || (false == 0);
 var logical4 = !0;
 var logical5;
 var logical6;
