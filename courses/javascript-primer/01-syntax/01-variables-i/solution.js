40df8eee4f15d7cfe91bd132d6d20b0a2dab40ce
Jared Rader <raderj89@gmail.com>
2016-09-01 11:43:41 -0700

    testing blync

--- courses/javascript-primer/01-syntax/01-variables-i/solution.js
+++ courses/javascript-primer/01-syntax/01-variables-i/solution.js
@@ -1,2 +1,2 @@
-<!--{ ids:[132], language:'JavaScript', type:'workshop', order: 0, name:'Variables I', description:'Create a variable and store data' }-->
+<!--{ ids:[132], language:'JavaScript', type:'workshop', order: 0, name:'Variables I', description:'Create variable and store data' }-->
 var numberOfStatesInUSA = 50;
